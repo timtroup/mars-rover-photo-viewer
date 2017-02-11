e84731e4f893c6e61b663a9d7aaa75be81b6c22a
troup <tim.troup@gmail.com>
2017-02-11 22:37:25 +0000

    Added react-datepicker and associated dependencies including ability to import css files via webpack

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
                 query: {
                     presets: ['es2015', 'stage-0']
                 }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
             }
         ]
     }
