4df6a49186493b8958bbc0720bd18170194762bc
troup <tim.troup@gmail.com>
2017-02-11 21:57:44 +0000

    Redux actions and reducers for storing photos json

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import manifestByRover from './manifest-reducer';
 import selectedRover from './select-rover-reducer';
 import selectedDate from './select-date-reducer';
 import selectedCamera from './select-camera-reducer';
+import photos from './photos-reducer';
 
 const rootReducer = combineReducers({
     manifestByRover,
     selectedRover,
     selectedDate,
-    selectedCamera
+    selectedCamera,
+    photos
 });
 
 export default rootReducer;
