62a4bc9cf90ab267ef460bde000e86d89dc49542
ttroup <Tim.Troup@tescobank.com>
2017-03-10 11:51:08 +0000

    First real test... Yes I should be doing TDD but at least we are now retrospectively plugging that gapping hole before taking the prototype any further

--- test/components/image-downloader.spec.js
+++ test/components/image-downloader.spec.js
@@ -1,5 +1,10 @@
-describe('a passing test', () => {
-    it('should pass', () => {
-        expect(true).to.be.true;
+import React from 'react';
+import ImageDownloader from '../../src/components/image-downloader';
+
+describe('<ImageDownloader />', () => {
+    const wrapper = shallow(<ImageDownloader />);
+
+    it('should be an anchor', () => {
+        expect(wrapper.type()).to.eql('a');
     });
 });
