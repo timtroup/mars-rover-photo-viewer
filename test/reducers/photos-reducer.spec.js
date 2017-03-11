3001d617863533eb1006c43b873f17620c58f3d3
troup <tim.troup@gmail.com>
2017-03-11 23:22:50 +0000

    Improve test coverage

--- test/reducers/photos-reducer.spec.js
+++ test/reducers/photos-reducer.spec.js
@@ -9,12 +9,24 @@ describe('photos reducer', () => {
         })
     });
 
-    it('should return correct state when ', () => {
+    it('should return correct state when REQUEST_PHOTOS', () => {
         expect(photos({}, {
             type: 'REQUEST_PHOTOS',
             rover: 'curiosity'
         })).to.eql({
             isFetching: true,
+        })
+    });
+
+    it('should return correct state when RECEIVE_PHOTOS', () => {
+        expect(photos({}, {
+            type: 'RECEIVE_PHOTOS',
+            photos: [{id: 1}],
+            receivedAt: 1458969960969
+        })).to.eql({
+            isFetching: false,
+            photos: [{id: 1}],
+            lastUpdated: 1458969960969
         })
     });
 });
