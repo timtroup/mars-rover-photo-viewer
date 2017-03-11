cfd6b0b838595d6dca806d8fcc8b83cb9b992cf2
troup <tim.troup@gmail.com>
2017-03-11 23:18:21 +0000

    Improve test coverage

--- test/reducers/preload-photo-reducer.spec.js
+++ test/reducers/preload-photo-reducer.spec.js
@@ -41,4 +41,48 @@ describe('photoGalleryData reducer', () => {
             lastUpdated: 1458969960969
         })
     });
+
+    it('should return correct state when RECEIVE_PRELOAD_PHOTOS', () => {
+        expect(photoGalleryData(
+            {
+                data: [{id: 1}],
+                isFetching: false
+            },
+            {
+                type: 'DELETE_PHOTOS'
+            }
+        )).to.eql({
+            data: [],
+            isFetching: false
+        })
+    });
+
+    it('should return correct state when RECEIVE_COLORIZE', () => {
+        let photo = {
+            id: 1,
+            lightboxImage: {}
+        };
+        expect(photoGalleryData(
+            {
+                data: [photo],
+                isFetching: false
+            },
+            {
+                type: 'RECEIVE_COLORIZE',
+                photo: photo,
+                base64Image: 'base64Image',
+                receivedAt: 1458969960969
+            }
+        )).to.eql({
+            data: [{
+                "id": 1,
+                "lightboxImage": {
+                    "src": "data:image/jpeg;base64,base64Image"
+                },
+                "src": "data:image/jpeg;base64,base64Image"
+            }
+        ],
+            isFetching: false
+        })
+    });
 });
