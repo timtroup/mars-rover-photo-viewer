118f78f94a44e1f96a04d8fce5dbef0e3594bee6
troup <tim.troup@gmail.com>
2017-03-11 11:25:27 +0000

    Pass props when calling shallow render to avoid warning messages

--- test/components/image-downloader.spec.js
+++ test/components/image-downloader.spec.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ImageDownloader from '../../src/components/image-downloader';
 
 describe('<ImageDownloader />', () => {
-    const wrapper = shallow(<ImageDownloader />);
+    const props = { imgSrc: 'http://somerledsolutions.com/img.jpeg' };
+    const wrapper = shallow(<ImageDownloader {...props}/>);
 
     it('should be an anchor', () => {
         expect(wrapper.type()).to.eql('a');
     });
 
     it('renders the correct img src', () => {
-        wrapper.setProps({ imgSrc: 'http://somerledsolutions.com' });
-        expect(wrapper.find({ href: 'http://somerledsolutions.com' })).to.have.length(1);
+        expect(wrapper.find({ href: 'http://somerledsolutions.com/img.jpeg' })).to.have.length(1);
     });
 });
